390074f3a3e6f6dd4042434517d6be628840e923
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2024-01-26 08:38:18 +0000

    "-Name" switch on export, comment handling

--- scripts/impexppsd1.ps1
+++ scripts/impexppsd1.ps1
@@ -17,6 +17,8 @@
 #
 # Export Usage:
 # $data | Export-PowerShellData1 -Path 'C:\path\to\your\file.psd1'
+# or
+# $data | Export-PowerShellData1 -Path 'C:\path\to\your\file.psd1' -Name
 
 # Function Import Psd1
 function Import-PowerShellData1 {
@@ -24,53 +26,53 @@ function Import-PowerShellData1 {
         [string]$Path
     )
     $content = Get-Content -Path $Path -Raw
+    $content = $content -replace '^(#.*[\r\n]*)+', '' # Remove lines starting with '#'
     $content = $content -replace '\bTrue\b', '$true' -replace '\bFalse\b', '$false'
     $scriptBlock = [scriptblock]::Create($content)
     $data = . $scriptBlock
-
     return $data
 }
 
 # Function Export Psd1
 function Export-PowerShellData1 {
-    [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
-        [hashtable]$Data,
-
         [Parameter(Mandatory = $true)]
-        [string]$Path
+        [hashtable]$Data,
+        [Parameter(Mandatory = $true)]
+        [string]$Path,
+        [switch]$Name
     )
-    process {
-        function ConvertTo-Psd1Content {
-            param ($Value)
-            switch ($Value) {
-                { $_ -is [System.Collections.Hashtable] } {
-                    "@{" + ($Value.GetEnumerator() | ForEach-Object {
-                        "`n    $($_.Key) = $(ConvertTo-Psd1Content $_.Value)"
-                    }) -join ";" + "`n}" + "`n"
-                }
-                { $_ -is [System.Collections.IEnumerable] -and $_ -isnot [string] } {
-                    "@(" + ($Value | ForEach-Object {
-                        ConvertTo-Psd1Content $_
-                    }) -join ", " + ")"
-                }
-                { $_ -is [PSCustomObject] } {
-                    $hashTable = @{}
-                    $_.psobject.properties | ForEach-Object { $hashTable[$_.Name] = $_.Value }
-                    ConvertTo-Psd1Content $hashTable
-                }
-                { $_ -is [string] } { "`"$Value`"" }
-                { $_ -is [int] -or $_ -is [long] -or $_ -is [bool] -or $_ -is [double] -or $_ -is [decimal] } { $_ }
-                default { 
-                    "`"$Value`"" 
-                }
+    function ConvertTo-Psd1Content {
+        param ($Value)
+        switch ($Value) {
+            { $_ -is [System.Collections.Hashtable] } {
+                "@{" + ($Value.GetEnumerator() | ForEach-Object {
+                    "`n    $($_.Key) = $(ConvertTo-Psd1Content $_.Value)"
+                }) -join ";" + "`n}" + "`n"
+            }
+            { $_ -is [System.Collections.IEnumerable] -and $_ -isnot [string] } {
+                "@(" + ($Value | ForEach-Object {
+                    ConvertTo-Psd1Content $_
+                }) -join ", " + ")"
+            }
+            { $_ -is [PSCustomObject] } {
+                $hashTable = @{}
+                $_.psobject.properties | ForEach-Object { $hashTable[$_.Name] = $_.Value }
+                ConvertTo-Psd1Content $hashTable
+            }
+            { $_ -is [string] } { "`"$Value`"" }
+            { $_ -is [int] -or $_ -is [long] -or $_ -is [bool] -or $_ -is [double] -or $_ -is [decimal] } { $_ }
+            default { 
+                "`"$Value`"" 
             }
         }
-        $psd1Content = "@{" + ($Data.GetEnumerator() | ForEach-Object {
-            "`n    $($_.Key) = $(ConvertTo-Psd1Content $_.Value)"
-        }) -join ";" + "`n" + "}"
-        Set-Content -Path $Path -Value $psd1Content -Force
     }
+    $fileName = if ($Name) { Split-Path $Path -Leaf } else { $null }
+    $psd1Content = if ($fileName) { "# Script: $fileName`n`n" } else { "" }
+    $psd1Content += "@{" + ($Data.GetEnumerator() | ForEach-Object {
+        "`n    $($_.Key) = $(ConvertTo-Psd1Content $_.Value)"
+    }) -join ";" + "`n" + "}"
+    Set-Content -Path $Path -Value $psd1Content -Force
 }
+
 
